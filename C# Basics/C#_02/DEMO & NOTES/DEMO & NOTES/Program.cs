b763c2928df738b2a9d04097d1334965c20252c4
Mina-Raouf <164073961+Mina-Raouf@users.noreply.github.com>
2024-12-11 21:12:42 +0200

    Add files via upload

---
+++ C# Basics/C#_02/DEMO & NOTES/DEMO & NOTES/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace DEMO___NOTES
+{
+    class Point
+    {
+        public int X, Y;
+       
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Error Types
+            // Sytnatx Error ---> In compile Time
+            // Runtime Error ---> In Runtime
+            // Logical Error ---> Un Excepected output
+            #endregion
+
+            #region Variable Declaration
+            //int x = 5;
+            //int y = x;
+            #endregion
+
+            #region Data Types
+            //double x = 100.153;
+            //string Name = "Mina";
+            #endregion
+
+            #region Value Types
+            // int ,double ,string ,struct 
+            // stored In Stack
+            #endregion
+
+            #region Reference Types
+            //Class ,String
+            //Stored In Heap
+            // Point p = new Point();
+            #endregion
+
+            #region Object
+            //int x = 10;
+            //Point p = new Point();
+            //object obj = x;
+            //obj = p;
+            
+            #endregion
+
+
+        }
+    }
+}
